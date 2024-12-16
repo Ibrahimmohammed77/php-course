42714eaf61c1950fb3e5111b4d0085f6ffcbff84
ibrahimi alshami <ibrahim77idalshami@gmail.com>
2024-12-16 21:13:36 +0300

    add lab1 lecture

---
+++ lab1/index.php
@@ -0,0 +1,86 @@
+<?php
+// Syntax
+
+// Comments
+#
+/*  */
+
+// PHP Variables
+$c="Ali";
+$boolean=false;
+
+# if statement
+if($boolean){
+
+}
+
+# String
+$gg="Salma";
+
+// Ineger
+$nyt=9;
+echo $gg +"<br>";
+// Declaring PHP Variables
+
+// Rules for PHP variables:
+
+//     A variable starts with the $ sign, followed by the name of the variable
+//     A variable name must start with a letter or the underscore character
+//     A variable name cannot start with a number
+//     A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+//     Variable names are case-sensitive ($age and $AGE are two different variables)
+
+
+// DataTypes
+// PHP supports the following data types:
+$b=null; //
+# if Statment
+if($b){
+
+}
+// String
+$name="Ali";
+echo $name+"<br>";
+// Integer
+$n=3;
+echo $n+"<br>";
+// Float (floating point numbers - also called double)
+// Boolean
+// Array
+// 
+$name=="Ali"? "Ali": "Abeer";
+// NULL
+// 
+
+// Get the Type
+// To get the data type of a variable, use the var_dump() function.
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+
+
+//OutPuts echo print
+
+
+// Global and Local Scope && Change Data Type && PHP Constants && Magic Constants
+
+// PHP Operators
+// Operators are used to perform operations on variables and values.
+# if sttement
+if(""){
+}
+// PHP divides the operators in the following groups:
+
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Increment/Decrement operators
+// Logical operators
+// String operators
+// Array operators
+// Conditional assignment operators
+
+echo "Youssef";
